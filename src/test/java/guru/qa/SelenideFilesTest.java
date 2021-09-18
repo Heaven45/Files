b91bbe43e2530606055b7e6c777ba2548adc8c8b
s.fedulov <s.fedulov@corp.vk.com>
2021-09-18 23:58:20 +0300

    Add docx file test

--- src/test/java/guru/qa/SelenideFilesTest.java
+++ src/test/java/guru/qa/SelenideFilesTest.java
@@ -2,15 +2,22 @@ package guru.qa;
 
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
+import net.lingala.zip4j.ZipFile;
 import org.junit.jupiter.api.Test;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+import java.io.File;
 import java.io.FileInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.time.Duration;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SelenideFilesTest {
@@ -40,20 +47,38 @@ public class SelenideFilesTest {
     }
 
     @Test
-    void zipFileTest() {
+    void zipFileTest() throws Exception {
 
+        ZipFile zipFile = new ZipFile("src/test/resources/test.zip");
+        if (zipFile.isEncrypted()) {
+            zipFile.setPassword("123".toCharArray());
+
+
+            try (InputStream is = getClass().getClassLoader().getResourceAsStream("test.zip")) {
+                ZipInputStream zis = new ZipInputStream(is);
+                ZipEntry entry;
+
+                while ((entry = zis.getNextEntry()) != null) {
+                    System.out.println(entry.getName());
+                }
+            }
+        }
     }
 
+    @Test
+    void docxFileTest() throws Exception {
+        try (InputStream fis = getClass().getClassLoader().getResourceAsStream("test.docx")) {
 
+            StringBuilder fullText = new StringBuilder();
+
+            XWPFDocument document = new XWPFDocument(fis);
+
+            List<XWPFParagraph> paragraphs = document.getParagraphs();
+
+            for (XWPFParagraph para : paragraphs) {
+                fullText.append(para.getText());
+            }
+            assertThat(fullText.toString()).contains("Отчет о проверке № 1");
+        }
+    }
 }
-//    1. Простая часть дз
-//
-//    Создайте свои файлы и напишите проверки содержимого:
-// — .txt
-//— .pdf
-//— .xls/.xlsx
-//— Архив .zip c паролем.
-//2. Посложнее
-//— Напишите проверки на .doc/.docx
-//
-//    В качестве ответа приложите ссылку на свой репозиторий в GitHub.
