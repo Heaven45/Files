26d20daa12e2704f6d22022ca8a9db8479bfcd9c
s.fedulov <s.fedulov@corp.vk.com>
2021-09-19 00:59:49 +0300

    Add zip with password test

--- src/test/java/guru/qa/SelenideFilesTest.java
+++ src/test/java/guru/qa/SelenideFilesTest.java
@@ -2,30 +2,26 @@ package guru.qa;
 
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
-import net.lingala.zip4j.ZipFile;
 import org.junit.jupiter.api.Test;
-
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-
-import java.io.File;
-import java.io.FileInputStream;
+import net.lingala.zip4j.exception.ZipException;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static utils.Files.readTextFromPath;
+import static utils.Zip.unzip;
 
 public class SelenideFilesTest {
 
     @Test
     void txtFileTest() throws Exception {
         String result;
-        try (InputStream is = getClass().getClassLoader().getResourceAsStream("test.txt")) {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("files/test.txt")) {
             result = new String(is.readAllBytes(), StandardCharsets.UTF_8);
         }
         assertThat(result).contains("Suspendisse convallis commodo");
@@ -33,13 +29,13 @@ public class SelenideFilesTest {
 
     @Test
     void pdfFileTest() throws Exception {
-        PDF parsed = new PDF(getClass().getClassLoader().getResourceAsStream("test.pdf"));
+        PDF parsed = new PDF(getClass().getClassLoader().getResourceAsStream("files/test.pdf"));
         assertThat(parsed.text).contains("Электронный\nбилет");
     }
 
     @Test
     void xlsFileTest() throws Exception {
-        try (InputStream stream = getClass().getClassLoader().getResourceAsStream("test.xlsx")) {
+        try (InputStream stream = getClass().getClassLoader().getResourceAsStream("files/test.xlsx")) {
             XLS parsed = new XLS(stream);
             assertThat(parsed.excel.getSheetAt(0).getRow(2).getCell(1).getStringCellValue())
                     .isEqualTo("А");
@@ -47,27 +43,23 @@ public class SelenideFilesTest {
     }
 
     @Test
-    void zipFileTest() throws Exception {
+    void zipFileWithPasswordTest() throws IOException, ZipException {
+        String zipFilePath = "./src/test/resources/files/test.zip";
+        String unzipFolderPath = "./src/test/resources/files/unzip";
+        String zipPassword = "123";
+        String unzipTxtFilePath = "./src/test/resources/files/unzip/test.txt";
+        String expectedData = "Funny";
 
-        ZipFile zipFile = new ZipFile("src/test/resources/test.zip");
-        if (zipFile.isEncrypted()) {
-            zipFile.setPassword("123".toCharArray());
+        unzip(zipFilePath, unzipFolderPath, zipPassword);
 
+        String actualData = readTextFromPath(unzipTxtFilePath);
 
-            try (InputStream is = getClass().getClassLoader().getResourceAsStream("test.zip")) {
-                ZipInputStream zis = new ZipInputStream(is);
-                ZipEntry entry;
-
-                while ((entry = zis.getNextEntry()) != null) {
-                    System.out.println(entry.getName());
-                }
-            }
-        }
+        assertThat(actualData).contains(expectedData);
     }
 
     @Test
     void docxFileTest() throws Exception {
-        try (InputStream fis = getClass().getClassLoader().getResourceAsStream("test.docx")) {
+        try (InputStream fis = getClass().getClassLoader().getResourceAsStream("files/test.docx")) {
 
             StringBuilder fullText = new StringBuilder();
 
